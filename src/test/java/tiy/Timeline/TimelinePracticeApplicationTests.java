2ded6958d2e3ba178bb5f106a88018d09ebc0a56
benjamin mcmichael <benjamin mcmichael>
2016-10-11 11:05:53 -0400

    fixed register error

--- src/test/java/tiy/Timeline/TimelinePracticeApplicationTests.java
+++ src/test/java/tiy/Timeline/TimelinePracticeApplicationTests.java
@@ -2,15 +2,42 @@ package tiy.Timeline;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+
+import static org.junit.Assert.*;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TimelinePracticeApplicationTests {
+
+	@Autowired
+	UserRepository users;
+
+	@Autowired
+	private RESTController testController;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void registerTest() throws Exception {
+		User testUser = new User("john doe", "password");
+		try {
+			testUser = testController.register(testUser);
+			testUser = users.findOne(testUser.getId());
+
+			assertNotNull(testUser);
+			assertEquals("john doe", testUser.username);
+		} catch (Exception ex){
+
+		} finally {
+			users.deleteAll();
+		}
 	}
 
 }
