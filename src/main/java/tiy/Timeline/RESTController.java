4b9d8aace9d77bcdb3293671be1e1e8d12cff8d5
benjamin mcmichael <benjamin mcmichael>
2016-10-25 11:19:10 -0400

    adding partial demodata endpoint

--- src/main/java/tiy/Timeline/RESTController.java
+++ src/main/java/tiy/Timeline/RESTController.java
@@ -266,148 +266,149 @@ public class RESTController {
         return timeBlocker.addTimeblocks(eventList);
     }
 
-//    public void createDemo() {
-//        events.deleteAll();
-//        contacts.deleteAll();
-//        users.deleteAll();
-//
-//        User John, Joan, Jude;
-//
-//        John = new User("john", "password", "john@email.com");
-//        Joan = new User("joan", "password", "joan@email.com");
-//        Jude = new User("jude", "password", "jude@email.com");
-//
-//        users.save(John);
-//        users.save(Joan);
-//        users.save(Jude);
-//
-//        ZonedDateTime today = ZonedDateTime.now(ZoneOffset.UTC);
-//        GregorianCalendar todayDate = GregorianCalendar.from(today);
-//        while (todayDate.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
-//            todayDate.add(Calendar.DATE, 1);
-//        }
-//        ZonedDateTime monday = todayDate.toZonedDateTime();
-//        monday = monday.withHour(0);
-//        monday = monday.withMinute(0);
-//        monday = monday.withSecond(0);
-//        monday = monday.withNano(0);
-//
-//        while (todayDate.get(Calendar.DAY_OF_WEEK) != Calendar.TUESDAY) {
-//            todayDate.add(Calendar.DATE, 1);
-//        }
-//        ZonedDateTime tuesday = todayDate.toZonedDateTime();
-//        tuesday = tuesday.withHour(0);
-//        tuesday = tuesday.withMinute(0);
-//        tuesday = tuesday.withSecond(0);
-//        tuesday = tuesday.withNano(0);
-//
-//        while (todayDate.get(Calendar.DAY_OF_WEEK) != Calendar.WEDNESDAY) {
-//            todayDate.add(Calendar.DATE, 1);
-//        }
-//        ZonedDateTime wednesday = todayDate.toZonedDateTime();
-//        wednesday = wednesday.withHour(0);
-//        wednesday = wednesday.withMinute(0);
-//        wednesday = wednesday.withSecond(0);
-//        wednesday = wednesday.withNano(0);
-//
-//        while (todayDate.get(Calendar.DAY_OF_WEEK) != Calendar.THURSDAY) {
-//            todayDate.add(Calendar.DATE, 1);
-//        }
-//        ZonedDateTime thursday = todayDate.toZonedDateTime();
-//        thursday = thursday.withHour(0);
-//        thursday = thursday.withMinute(0);
-//        thursday = thursday.withSecond(0);
-//        thursday = thursday.withNano(0);
-//
-//        while (todayDate.get(Calendar.DAY_OF_WEEK) != Calendar.FRIDAY) {
-//            todayDate.add(Calendar.DATE, 1);
-//        }
-//        ZonedDateTime friday = todayDate.toZonedDateTime();
-//        friday = friday.withHour(0);
-//        friday = friday.withMinute(0);
-//        friday = friday.withSecond(0);
-//        friday = friday.withNano(0);
-//
-//        while (todayDate.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY) {
-//            todayDate.add(Calendar.DATE, 1);
-//        }
-//        ZonedDateTime saturday = todayDate.toZonedDateTime();
-//        saturday = saturday.withHour(0);
-//        saturday = saturday.withMinute(0);
-//        saturday = saturday.withSecond(0);
-//        saturday = saturday.withNano(0);
-//
-//        while (todayDate.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
-//            todayDate.add(Calendar.DATE, 1);
-//        }
-//        ZonedDateTime sunday = todayDate.toZonedDateTime();
-//        sunday = sunday.withHour(0);
-//        sunday = sunday.withMinute(0);
-//        sunday = sunday.withSecond(0);
-//        sunday = sunday.withNano(0);
-//        ZonedDateTime tempdate;
-//        //MONDAY
-//        //John
-//        Event event = new Event("work", monday.plusHours(9), monday.plusHours(17), ZoneId.systemDefault().toString(), "Casa de Waffle", false, John);
-//        events.save(event);
-//        tempdate = monday.plusHours(17);
-//        tempdate = tempdate.plusMinutes(30);
-//        event = new Event("Vet visit", tempdate, monday.plusHours(19), ZoneId.systemDefault().toString(), "spike's surgery is today, can't miss this guys", false, John);
-//        events.save(event);
-//        //Joan
-//        event = new Event("work", monday.plusHours(23), monday.plusHours(31), ZoneId.systemDefault().toString(), "On Call at tech support", false, Joan);
-//        events.save(event);
-//
-//        //TUESDAY
-//        //John
-//        event = new Event("work", tuesday.plusHours(9), tuesday.plusHours(17), ZoneId.systemDefault().toString(), "Casa de Waffle", false, John);
-//        events.save(event);
-//        //Joan
-//        event = new Event("work", tuesday.plusHours(23), tuesday.plusHours(31), ZoneId.systemDefault().toString(), "On Call at tech support", false, Joan);
-//        events.save(event);
-//
-//        //WEDNESDAY
-//        //John
-//        event = new Event("work", wednesday.plusHours(9), wednesday.plusHours(17), ZoneId.systemDefault().toString(), "Casa de Waffle", false, John);
-//        events.save(event);
-//        event = new Event("choir practice", wednesday.plusHours(18), wednesday.plusHours(21), ZoneId.systemDefault().toString(), "I added an hour for the drive home guys", false, John);
-//        events.save(event);
-//        //Joan
-//        event = new Event("work", wednesday.plusHours(23), wednesday.plusHours(31), ZoneId.systemDefault().toString(), "On Call at tech support", false, Joan);
-//        events.save(event);
-//        //THURSDAY
-//        //John
-//        event = new Event("work", thursday.plusHours(9), thursday.plusHours(17), ZoneId.systemDefault().toString(), "Casa de Waffle", false, John);
-//        events.save(event);
-//        //Joan
-//        event = new Event("work", thursday.plusHours(23), thursday.plusHours(31), ZoneId.systemDefault().toString(), "On Call at tech support", false, Joan);
-//        events.save(event);
-//        //FRIDAY
-//        //John
-//        event = new Event("work", friday.plusHours(9), friday.plusHours(17), ZoneId.systemDefault().toString(), "Casa de Waffle", false, John);
-//        events.save(event);
-//        //Joan
-//        event = new Event("work", friday.plusHours(23), friday.plusHours(31), ZoneId.systemDefault().toString(), "On Call at tech support", false, Joan);
-//        events.save(event);
-//        //SATURDAY
-//        //John
-//
-//        //SUNDAY
-//        //John
-//        event = new Event("church", sunday.plusHours(8), sunday.plusHours(11), ZoneId.systemDefault().toString(), "Singing in the choir if you guys want to come see", false, John);
-//        events.save(event);
-//
-////        System.out.println(monday.toString());
-////        System.out.println(tuesday.toString());
-////        System.out.println(wednesday.toString());
-////        System.out.println(thursday.toString());
-////        System.out.println(friday.toString());
-////        System.out.println(saturday.toString());
-////        System.out.println(sunday.toString());
-//        //Event JohnsWork = new Event("Work", Date)
-//
-//
-////    @RequestMapping(path = "/contacts", method = RequestMethod.POST)
-//
-//    }
+    @RequestMapping(path = "/demo", method = RequestMethod.POST)
+    public void createDemo() {
+        events.deleteAll();
+        contacts.deleteAll();
+        users.deleteAll();
+
+        User John, Joan, Jude;
+
+        John = new User("john", "password", "john@email.com");
+        Joan = new User("joan", "password", "joan@email.com");
+        Jude = new User("jude", "password", "jude@email.com");
+
+        users.save(John);
+        users.save(Joan);
+        users.save(Jude);
+
+        ZonedDateTime today = ZonedDateTime.now(ZoneOffset.UTC);
+        GregorianCalendar todayDate = GregorianCalendar.from(today);
+        while (todayDate.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
+            todayDate.add(Calendar.DATE, 1);
+        }
+        ZonedDateTime monday = todayDate.toZonedDateTime();
+        monday = monday.withHour(0);
+        monday = monday.withMinute(0);
+        monday = monday.withSecond(0);
+        monday = monday.withNano(0);
+
+        while (todayDate.get(Calendar.DAY_OF_WEEK) != Calendar.TUESDAY) {
+            todayDate.add(Calendar.DATE, 1);
+        }
+        ZonedDateTime tuesday = todayDate.toZonedDateTime();
+        tuesday = tuesday.withHour(0);
+        tuesday = tuesday.withMinute(0);
+        tuesday = tuesday.withSecond(0);
+        tuesday = tuesday.withNano(0);
+
+        while (todayDate.get(Calendar.DAY_OF_WEEK) != Calendar.WEDNESDAY) {
+            todayDate.add(Calendar.DATE, 1);
+        }
+        ZonedDateTime wednesday = todayDate.toZonedDateTime();
+        wednesday = wednesday.withHour(0);
+        wednesday = wednesday.withMinute(0);
+        wednesday = wednesday.withSecond(0);
+        wednesday = wednesday.withNano(0);
+
+        while (todayDate.get(Calendar.DAY_OF_WEEK) != Calendar.THURSDAY) {
+            todayDate.add(Calendar.DATE, 1);
+        }
+        ZonedDateTime thursday = todayDate.toZonedDateTime();
+        thursday = thursday.withHour(0);
+        thursday = thursday.withMinute(0);
+        thursday = thursday.withSecond(0);
+        thursday = thursday.withNano(0);
+
+        while (todayDate.get(Calendar.DAY_OF_WEEK) != Calendar.FRIDAY) {
+            todayDate.add(Calendar.DATE, 1);
+        }
+        ZonedDateTime friday = todayDate.toZonedDateTime();
+        friday = friday.withHour(0);
+        friday = friday.withMinute(0);
+        friday = friday.withSecond(0);
+        friday = friday.withNano(0);
+
+        while (todayDate.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY) {
+            todayDate.add(Calendar.DATE, 1);
+        }
+        ZonedDateTime saturday = todayDate.toZonedDateTime();
+        saturday = saturday.withHour(0);
+        saturday = saturday.withMinute(0);
+        saturday = saturday.withSecond(0);
+        saturday = saturday.withNano(0);
+
+        while (todayDate.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
+            todayDate.add(Calendar.DATE, 1);
+        }
+        ZonedDateTime sunday = todayDate.toZonedDateTime();
+        sunday = sunday.withHour(0);
+        sunday = sunday.withMinute(0);
+        sunday = sunday.withSecond(0);
+        sunday = sunday.withNano(0);
+        ZonedDateTime tempdate;
+        //MONDAY
+        //John
+        Event event = new Event("work", monday.plusHours(9), monday.plusHours(17), ZoneId.systemDefault().toString(), "Casa de Waffle", false, John);
+        events.save(event);
+        tempdate = monday.plusHours(17);
+        tempdate = tempdate.plusMinutes(30);
+        event = new Event("Vet visit", tempdate, monday.plusHours(19), ZoneId.systemDefault().toString(), "spike's surgery is today, can't miss this guys", false, John);
+        events.save(event);
+        //Joan
+        event = new Event("work", monday.plusHours(23), monday.plusHours(31), ZoneId.systemDefault().toString(), "On Call at tech support", false, Joan);
+        events.save(event);
+
+        //TUESDAY
+        //John
+        event = new Event("work", tuesday.plusHours(9), tuesday.plusHours(17), ZoneId.systemDefault().toString(), "Casa de Waffle", false, John);
+        events.save(event);
+        //Joan
+        event = new Event("work", tuesday.plusHours(23), tuesday.plusHours(31), ZoneId.systemDefault().toString(), "On Call at tech support", false, Joan);
+        events.save(event);
+
+        //WEDNESDAY
+        //John
+        event = new Event("work", wednesday.plusHours(9), wednesday.plusHours(17), ZoneId.systemDefault().toString(), "Casa de Waffle", false, John);
+        events.save(event);
+        event = new Event("choir practice", wednesday.plusHours(18), wednesday.plusHours(21), ZoneId.systemDefault().toString(), "I added an hour for the drive home guys", false, John);
+        events.save(event);
+        //Joan
+        event = new Event("work", wednesday.plusHours(23), wednesday.plusHours(31), ZoneId.systemDefault().toString(), "On Call at tech support", false, Joan);
+        events.save(event);
+        //THURSDAY
+        //John
+        event = new Event("work", thursday.plusHours(9), thursday.plusHours(17), ZoneId.systemDefault().toString(), "Casa de Waffle", false, John);
+        events.save(event);
+        //Joan
+        event = new Event("work", thursday.plusHours(23), thursday.plusHours(31), ZoneId.systemDefault().toString(), "On Call at tech support", false, Joan);
+        events.save(event);
+        //FRIDAY
+        //John
+        event = new Event("work", friday.plusHours(9), friday.plusHours(17), ZoneId.systemDefault().toString(), "Casa de Waffle", false, John);
+        events.save(event);
+        //Joan
+        event = new Event("work", friday.plusHours(23), friday.plusHours(31), ZoneId.systemDefault().toString(), "On Call at tech support", false, Joan);
+        events.save(event);
+        //SATURDAY
+        //John
+
+        //SUNDAY
+        //John
+        event = new Event("church", sunday.plusHours(8), sunday.plusHours(11), ZoneId.systemDefault().toString(), "Singing in the choir if you guys want to come see", false, John);
+        events.save(event);
+
+//        System.out.println(monday.toString());
+//        System.out.println(tuesday.toString());
+//        System.out.println(wednesday.toString());
+//        System.out.println(thursday.toString());
+//        System.out.println(friday.toString());
+//        System.out.println(saturday.toString());
+//        System.out.println(sunday.toString());
+        //Event JohnsWork = new Event("Work", Date)
+
+
+//    @RequestMapping(path = "/contacts", method = RequestMethod.POST)
+
+    }
 }
